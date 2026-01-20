d096671b532ba240c03290461397493704b7e9df
sahilsonkamble <sahil.sonkamble@pzwind.com>
2026-01-20 14:38:39 +0530

    modified app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import NavBar from './components/NavBar'
 const App = () => {
   return (
     <div>
+      <p>this is a basic time series plotting tool</p>
       <NavBar/>
       <div>
       <HomePage/>
