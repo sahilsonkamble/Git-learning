693c4ce6d0aba2ba9a844926a1af5d67d50e5f96
sahilsonkamble <sahil.sonkamble@pzwind.com>
2026-01-20 14:33:51 +0530

    create file-upload component

---
+++ src/components/FileUpload.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FileUpload = () => {
+  return (
+    <div>This is a FileUpload component</div>
+  )
+}
+
+export default FileUpload
--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
-import Chart from './Chart'
 
+import Chart from './Chart'
+import FileUpload from './FileUpload'
 const HomePage = () => {
   return (
    <div>
     <div>this is a HomePage</div>
     <div><Chart/></div>
+    <div><FileUpload/></div>
+
    </div>
   )
 }
