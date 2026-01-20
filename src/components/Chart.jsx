de12454ea74cca4d88a7904f3dd9ab81e2e1a9fe
sahilsonkamble <sahil.sonkamble@pzwind.com>
2026-01-20 14:29:57 +0530

    create a chart component

---
+++ src/components/Chart.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Chart = () => {
+  return (
+    <div style={{backgroundColor:"green"}}>this si a chart component</div>
+  )
+}
+
+export default Chart
--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import Chart from './Chart'
 
 const HomePage = () => {
   return (
-    <div>THis si the HomePage</div>
+   <div>
+    <div>this is a HomePage</div>
+    <div><Chart/></div>
+   </div>
   )
 }
 
